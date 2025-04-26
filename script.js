e60e81c6c83e7faed9b35371ae0c4f186f7d1aa3
Raman Gupta <111919201+rioxraman@users.noreply.github.com>
2025-04-26 20:08:02 +0000

    asmita friends

--- script.js
+++ script.js
@@ -11,14 +11,20 @@ setTimeout(() => {
     document.getElementById('discount-popup').style.display = 'block';
 }, 3000);
 
-document.querySelector('.close-btn').addEventListener('click', () => {
-    document.getElementById('discount-popup').style.display = 'none';
-});
+// document.querySelector('.close-btn').addEventListener('click', () => {
+//     document.getElementById('discount-popup').style.display = 'none';
+// });
 
-document.querySelector('.close-now-btn').addEventListener('click', () => {
-    document.getElementById('discount-popup').style.display = 'none';
-});
+// document.querySelector('.close-now-btn').addEventListener('click', () => {
+//     document.getElementById('discount-popup').style.display = 'none';
+// });
 
 document.querySelector('.contact-btn').addEventListener('click', () => {
-    window.location.href = 'https://wa.me/919056230369';
+    window.location.href = 'https://api.whatsapp.com/send?phone=919056230369&text=i%20am%20a%20plus1%20%student';
+});
+document.querySelector('.close-now-btn').addEventListener('click', () => {
+    window.location.href = 'https://api.whatsapp.com/send?phone=919056230369&text=i%20am%20a%20plus1%20%student';
+});
+document.querySelector('.close-btn').addEventListener('click', () => {
+    window.location.href = 'https://api.whatsapp.com/send?phone=919056230369&text=i%20am%20a%20plus1%20%student';
 });
